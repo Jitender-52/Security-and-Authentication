be910196fe88e17d4e3f3c65e7677d08f22448a0
Jitender <jitendersharmateharki@gmail.com>
2022-09-17 18:50:52 +0530

    add New PORT method

--- app.js
+++ app.js
@@ -95,6 +95,12 @@ app.post("/login", function (req, res) {
   //   });
 });
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log("Server has started on port 3000");
+// app.listen(process.env.PORT || 3000, function () {
+//   console.log("Server has started on port 3000");
+// });
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+    console.log("Server started succesfully");
 });
